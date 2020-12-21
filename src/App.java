62bb391c5cbbb30f2f6f089d36aa4b28ab759122
Hiroshi KATAOKA <e205745@ie.u-ryukyu.ac.jp>
2020-12-22 00:48:45 +0900

    例外処理追加

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String str = null;
-        str.length();
+        try{
+            String str = null;
+            str.length();
+        }catch(NullPointerException e){
+            System.out.println("例外 NullPointerException が発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
